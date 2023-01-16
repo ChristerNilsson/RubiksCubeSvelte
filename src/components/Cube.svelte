679fe498b3e2fe07b42439ea0780d85f42f1079c
ChristerNilsson <janchrister.nilsson@gmail.com>
2023-01-16 17:56:18 +0100

    x

--- src/components/Cube.svelte
+++ src/components/Cube.svelte
@@ -9,26 +9,29 @@
 	import { TaskQueue } from "../TaskQueue.js"
 	import { cubieList } from "../cubieList.js"
 	import { faceNames, layerMap } from "../layers.js"
-	import { mod, sleep, randEl } from "../utils.js"
+	import { log, mod, sleep, randEl } from "../utils.js"
+
+	let tops = [[3,5,6],[3,5,4],[1,0,5],[7,5,4],[7,3,6],[1,4,3]]
 
 	const ITERATIONS=50 // 50
 	// variables
 
 	export let popup
-	export let state
+	// export let state
 	export let cubeRotation
+	export let invHash
 
 	let visible = false
 	let scrambling = false
 	let transparent = false
 	let zoomFactor = 1
 	// $: rotationSpeed = scrambling ? 1*100 : 1*250
-	$: rotationSpeed = scrambling ? 5*50 : 5*125
-	let [x,y,z] = [0,0,0] // 3,5,6
-	cubeRotation = { x: 45*x, y: 45*y, z: 45*z }
+	$: rotationSpeed = scrambling ? 0 : 0 //5*125
+	// let [x,y,z] = [0,0,0] // 3,5,6
+	// cubeRotation = { x: x, y: y, z: z }
 	let layerTransform = ""
 
-	// let state = 'RBVOGY' // Röd Blå Vit Orange Grön Yellow
+	// let state = 'RBWOGY' // Red Blue White Orange Green Yellow
 
 	// cubies
 
@@ -63,97 +66,83 @@
 		}
 	}
 
+	const hash = {
+		BYO : 101, // 200,544,545,645],
+		BOW : 103, // 547,546,646],
+		BWR : 105, // 541,540,640],
+		BRY : 107, // 542,543,206,642,643],
 
+		GOY : 141, // 505,142,242,316],
+		GRW : 145, // 146,245,246,356,501],
+		GWO : 143, // 507,244,144,336],
+		GYR : 147, // 140,240,503,504,604],
+		
+		OWB : 716, // 25, 26,124,125,224,332,421,521,716,726],
+		OGW : 736, // 746,46],
+		OYG : 756, // 66,372,756,361,362,160,260,666,766],
+		OBY : 776, // 352,776,442,342,706],
 
+		RGY : 316, //732,306,406,42,742],
+		RYB : 336, // ,21,712,120,121,326,425,426,524,624],
+		RBW : 356, //,772,446,346],
+		RWG : 376, //,366,365,265,165,466,752],
 
-	// const hash = {}
-	// 						// lr ud pg
+		WGR : 314, // 40, 404,730,740],
+		WRB : 334, // 126,127,324,424,621,710,721],
+		WBO : 354, //  0,344,444,700,770],
+		WOG : 374, //  60,161,363,364,566,750,760],
 
-	// hash.BYOGVR =  "xxx"
-	// hash.BOVGRY =  "xxx"
-	// hash.BVRGYO =  "xxx"
-	// hash.BRYGOV =  "xxx"
+		YOB : 714, // 123,122,321,330,714,724,725],
+		YGO : 734, //300,400],
+		YRG : 754, // 166,370,754,360,367,460],
+		YBR : 774, //  4,350,704,774,340,440],
+	}
 
-	// hash.GRVBOY =  "xzx"
-	// hash.GYRBVO =  "xxx"
-	// hash.GOYBRV =  "xxx"
-	// hash.GVOBYR =  "xxx"
-
-	// hash.OGVRBY =  "xxx"
-	// hash.OVBRYG =  "xxx"
-	// hash.OBYRGV =  "xzy"
-	// hash.OYGRVB =  "xxx"
-
-	// hash.RGYOBV =  "xxx"
-	// hash.RYBOVG =  "xxx"
-	// hash.RBVOGY =  "xzy"
-	// hash.RVGOYB =  "xxx"
-
-	// hash.VGRYBO =  "xxx"
-	// hash.VRBYOG =  "xxx"
-	// hash.VBOYGR =  "xzy"
-	// hash.VOGYRB =  "xxx"
-
-	// hash.YGOVBR =  "xxx"
-	// hash.YOBVRG =  "xxx"
-	// hash.YBRVGO =  "xxy"
-	// hash.YRGVOB =  "xxx"
+	invHash = {}
+	for (const key in hash) {
+		const nr = hash[key]
+		invHash[nr] = key
+		invHash = invHash
+	}
+	console.log('Cube',{invHash})
 
 	const rot = (key,lst) => lst.map((i) => key[i]).join('')
 
-	function rotateCube(direction) { // t ex GVOBYR
+	let topIndex = 0
+	$: cubeRotation.x = tops[topIndex][0]
+	$: cubeRotation.y = tops[topIndex][1]
+	$: cubeRotation.z = tops[topIndex][2]
 
-		console.log(state)
+	function rotateCube(direction) { // t ex GWOBYR
 
-		// let [lr,ud,pg] = hash[state]
-		// console.log({state,lr,ud,pg})
-		// console.log(hash[state])
-		let pg
-		let lr
-		let ud
-
-		if ('BG'.includes(state[0])) pg='x'
-		if ('YV'.includes(state[0])) pg='y'
-		if ('OR'.includes(state[0])) pg='z'
-
-		if ('BG'.includes(state[1])) lr='x'
-		if ('YV'.includes(state[1])) lr='y'
-		if ('OR'.includes(state[1])) lr='z'
-
-		if ('BG'.includes(state[2])) ud='x'
-		if ('YV'.includes(state[2])) ud='y'
-		if ('OR'.includes(state[2])) ud='z'
-
-		console.log({pg,lr,ud})
-
-
-		if (direction == "left")  cubeRotation[lr] += 2
-		if (direction == "right") cubeRotation[lr] -= 2
-
-		if (direction == "up")    cubeRotation[ud] += 2
-		if (direction == "down")  cubeRotation[ud] -= 2
-
-		if (direction == "pu")    cubeRotation[pg] += 2
-		if (direction == "pd")    cubeRotation[pg] -= 2
-
-		let vector
+		let {x,y,z} = cubeRotation
+		let nr = 100*x+10*y+z
+		const state = invHash[nr]
 
 		if (direction == "left") {
-			if (lr=='x') vector = [0,5,1,3,2,4]
-			if (lr=='y') vector = [5,1,0,2,4,3]
-			if (lr=='z') vector = [1,3,2,4,0,5]
-		} 
-		if (direction == "right") vector = [0,5,1,3,2,4] // 0 3
-		if (direction == "right") vector = [0,2,4,3,5,1] // 0 3
+			if (x==1) cubeRotation.z += 2
+			else cubeRotation.y +=2
+		}
+		if (direction == "right") {
+			if (x==1) cubeRotation.z -= 2
+			else cubeRotation.y -=2
+		}
 
-		if (direction == "pu")    vector = [5,1,0,2,4,3] // 1 4
-		if (direction == "pd")    vector = [2,1,3,5,4,0] // 1 4
+		if (direction == "up")   topIndex = mod(topIndex+1,6)
+		if (direction == "down") topIndex = mod(topIndex-1,6)
 
-		if (direction == "down")  vector = [1,3,2,4,0,5] // 2 5
-		if (direction == "up")    vector = [4,0,2,1,3,5] // 2 5
+		log(topIndex)
 
-		state = rot(state,vector)
-	
+		// [x,y,z] = tops[topIndex]
+		// cubeRotation = {x,y,z}
+		// log(x,y,z,topIndex)
+		// x = mod(x,8)
+		// y = mod(y,8)
+		// z = mod(z,8)
+		// const nr = 100*x + 10*y + z
+		// state = invHash && invHash[nr] ? invHash[nr] : ""
+		// console.log('Cube',{x,y,z,nr,cubeRotation})
+
 	}
 
 	function zoom(factor) {
@@ -163,7 +152,7 @@
 	function toggleTransparency() {
 		transparent = !transparent;
 		console.log({cubeRotation})
-		console.log({state})
+		// console.log({state})
 	}
 
 	async function rotateLayer({ layer, orientation }) {
@@ -251,39 +240,50 @@
 	// key controller
 
 	const keyController = {
+
 		ArrowLeft:  [rotateCube, "left"],
 		ArrowRight: [rotateCube, "right"],
 		ArrowUp:    [rotateCube, "up"],
 		ArrowDown:  [rotateCube, "down"],
 		PageUp:     [rotateCube, "pu"],
 		PageDown:   [rotateCube, "pd"],
+
 		"+": [zoom, 1.15],
 		"-": [zoom, 1 / 1.15],
+
 		c: [toggleTransparency],
 		z: [undoRotation],
 		Z: [resetCube],
+
 		X: [scrambleCube],
 		x: [stopScrambling],
-		f: [addRotation, { layer: "front", orientation: "+" }],
-		F: [addRotation, { layer: "front", orientation: "-" }],
-		b: [addRotation, { layer: "back", orientation: "-" }],
-		B: [addRotation, { layer: "back", orientation: "+" }],
-		// t: [addRotation, { layer: "top", orientation: "-" }],
-		// T: [addRotation, { layer: "top", orientation: "+" }],
-		u: [addRotation, { layer: "top", orientation: "-" }],
-		U: [addRotation, { layer: "top", orientation: "+" }],
-		d: [addRotation, { layer: "down", orientation: "+" }],
-		D: [addRotation, { layer: "down", orientation: "-" }],
-		l: [addRotation, { layer: "left", orientation: "-" }],
-		L: [addRotation, { layer: "left", orientation: "+" }],
-		r: [addRotation, { layer: "right", orientation: "+" }],
-		R: [addRotation, { layer: "right", orientation: "-" }],
+
+		b: [addRotation, { layer: "front", orientation: "+" }],
+		B: [addRotation, { layer: "front", orientation: "-" }],
+
+		g: [addRotation, { layer: "back", orientation: "-" }],
+		G: [addRotation, { layer: "back", orientation: "+" }],
+
+		w: [addRotation, { layer: "top", orientation: "-" }],
+		W: [addRotation, { layer: "top", orientation: "+" }],
+
+		y: [addRotation, { layer: "down", orientation: "+" }],
+		Y: [addRotation, { layer: "down", orientation: "-" }],
+
+		r: [addRotation, { layer: "left", orientation: "-" }],
+		R: [addRotation, { layer: "left", orientation: "+" }],
+
+		o: [addRotation, { layer: "right", orientation: "+" }],
+		O: [addRotation, { layer: "right", orientation: "-" }],
+
 		// m: [addRotation, { layer: "middle", orientation: "-" }],
 		// M: [addRotation, { layer: "middle", orientation: "+" }],
 		// s: [addRotation, { layer: "standing", orientation: "+" }],
 		// S: [addRotation, { layer: "standing", orientation: "-" }],
 		// e: [addRotation, { layer: "equator", orientation: "+" }],
 		// E: [addRotation, { layer: "equator", orientation: "-" }],
+		// t: [addRotation, { layer: "top", orientation: "-" }],
+		// T: [addRotation, { layer: "top", orientation: "+" }],
 	};
 
 	const macros = {
@@ -313,13 +313,13 @@
 		console.log(ch,index)
 		assert([0,1,2,3,4,5].includes(index),true)
 		const color = state[index]
-		assert("RBVOGY".includes(color),true)
+		// assert("RBWOGY".includes(color),true)
 		let command = T1[color]
 		assert("LFURBD".includes(command),true)
 		if (lower) command = command.toLowerCase()
 		return command
 	}
-	assert( convert('RBVOGY','R'), 'U')
+	// assert( convert('RBWOGY','R'), 'U')
 
 	function enableKeyControl() {
 		document.addEventListener("keydown", (e) => {
@@ -329,7 +329,7 @@
 				const sequence = macros[key]
 				for (let ch of sequence) {
 
-					ch = convert(state,ch)
+					// ch = convert(state,ch)
 
 					console.log(ch)
 					const [fun, arg] = keyController[ch]
@@ -351,10 +351,55 @@
 		visible = true;
 		enableKeyControl();
 	});
+
+	let startX=undefined
+	let startY=undefined
+	let cx
+	let cy
+	let ddx=0
+	let ddy=0
+
+	// function md(e) {
+	// 	startX = e.clientX/5
+	// 	startY = e.clientY/5
+	// 	cx = cubeRotation.x
+	// 	cy = cubeRotation.y
+	// 	console.log('md',e.clientX,cubeRotation.x)
+	// }
+
+	// function mm(e) {
+	// 	if (startX==undefined) return
+	// 	const dx = e.clientX/5 - startX
+	// 	const dy = e.clientY/5 -startY
+	// 	if (Math.abs(ddx-dx)>10) {
+	// 		cubeRotation.x = cx+dx
+	// 		ddx=dx
+	// 	}
+	// 	if (Math.abs(ddy-dy)>10) {
+	// 		cubeRotation.y = cy+dy
+	// 		ddy=dy
+	// 	}
+	// }
+
+	// function mu(e) {
+	// 	startX=undefined
+	// 	startY=undefined
+	// 	console.log('mu')
+	// }
+
 </script>
 
+<!-- <input type="range" id="x" name="rotx" min="0" step=10 max="360" bind:value={cubeRotation.x}>
+<label for="rotx">X</label>
+<input type="range" id="y" name="roty" min="0" step=10 max="360" bind:value={cubeRotation.y}>
+<label for="roty">Y</label>
+<input type="range" id="z" name="rotz" min="0" step=10 max="360" bind:value={cubeRotation.z}>
+<label for="rotz">Z</label> -->
+
+<!-- on:mousedown={md} on:mousemove={mm} on:mouseup={mu} -->
+
 {#if visible}
-	<main in:fade={{ duration: 10 }}>
+	<main in:fade={{ duration: 0 }}  >
 		<div
 			class="cube"
 			style:--speed="{rotationSpeed}ms"
@@ -381,7 +426,11 @@
 	</main>
 {/if}
 
+
 <style>
+	input {
+		width:300px
+	}
 	main {
 		--cubie-size: min(120px, 18vw);
 		min-height: 100vh;
